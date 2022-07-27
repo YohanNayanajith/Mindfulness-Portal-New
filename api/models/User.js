63fb9ad4869596b27000dd319b60a6290f00babc
YohanNayanajith <yohannayanajith40@gmail.com>
2022-07-27 23:51:14 +0530

    User data edit

--- api/models/User.js
+++ api/models/User.js
@@ -3,11 +3,20 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
+    fullName: { type: String, required: true },
+    email: { type: String, required: true },
     password: { type: String, required: true },
+    confirmPassword: { type: String, required: true },
+    phoneNo: { type: String, required: true },
+    address: { type: String, required: true },
+    dateOfBirth: { type: Date, required: true },
     isAdmin: {
       type: Boolean,
       default: false,
+    },
+    isActivated: {
+      type: Boolean,
+      default: true,
     },
   },
   { timestamps: true }
