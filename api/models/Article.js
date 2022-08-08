9151af2a5923b3f6fa4185ee9788fa6e7a4db40a
YohanNayanajith <yohannayanajith40@gmail.com>
2022-08-08 23:52:33 +0530

    payment bug set

--- api/models/Article.js
+++ api/models/Article.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 
 const ArticleSchema = new mongoose.Schema(
   {
-    title: { type: String, required: true, unique: true },
+    title: { type: String, required: true },
     desc: { type: String, required: true, },
     img: { type: String, required: true },
     categories: { type: Array },
-    price: { type: Number, required: true },
+    price: { type: Number, default: 0 },
     inStock: { type: Boolean, default: true },
     isActivate: { type: Boolean, default: true },  
   },
--- api/models/Order.js
+++ api/models/Order.js
@@ -3,29 +3,26 @@ const mongoose = require("mongoose");
 const OrderSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true },
-    products: [
-      {
-        productId: {
-          type: String,
-        },
-        quantity: {
-          type: Number,
-          default: 1,
-        },
-        title:{
-          type: String,
-        },
-        desc:{
-          type: String,
-        },
-        img:{
-          type: String,
-        }
-      },
-    ],
-    amount: { type: Number, required: true },
-    address: { type: Object, required: true },
-    status: { type: String, default: "pending" },
+    productId: {
+      type: String,
+    },
+    quantity: {
+      type: Number,
+      default: 1,
+    },
+    title: {
+      type: String,
+    },
+    desc: {
+      type: String,
+    },
+    img: {
+      type: String,
+    },
+    price: { type: Number },
+    isPay: { type: Boolean, default: false },
+    isCancel: { type: Boolean, default: false },
+    status: { type: String, default: "Pending" },
   },
   { timestamps: true }
 );
